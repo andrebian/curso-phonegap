ed0c03a77a30c9ff1c0ba8efb27f12522babfffe
Andre Cardoso <andrecardosodev@gmail.com>
2013-12-14 14:34:12 -0200

    Adicionados plugins para localização e removidos elementos de contato que não funcionaram

--- Aula1/platforms/android/.staging/www/cordova_plugins.js
+++ Aula1/platforms/android/.staging/www/cordova_plugins.js
@@ -62,13 +62,57 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.contacts": "0.2.6",
-    "org.apache.cordova.device": "0.2.5"
+    "org.apache.cordova.device": "0.2.5",
+    "org.apache.cordova.geolocation": "0.3.4",
+    "org.apache.cordova.device-motion": "0.2.4"
 }
 // BOTTOM OF METADATA
 });
--- Aula1/platforms/firefoxos/.staging/www/cordova_plugins.js
+++ Aula1/platforms/firefoxos/.staging/www/cordova_plugins.js
@@ -72,13 +72,67 @@ module.exports = [
         "file": "plugins/org.apache.cordova.contacts/src/firefoxos/ContactsProxy.js",
         "id": "org.apache.cordova.contacts.ContactsProxy",
         "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/src/firefoxos/GeolocationProxy.js",
+        "id": "org.apache.cordova.geolocation.GeolocationProxy",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
+        "clobbers": [
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/src/firefoxos/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer-impl",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "org.apache.cordova.device": "0.2.5",
-    "org.apache.cordova.contacts": "0.2.6"
+    "org.apache.cordova.contacts": "0.2.6",
+    "org.apache.cordova.geolocation": "0.3.4",
+    "org.apache.cordova.device-motion": "0.2.4"
 }
 // BOTTOM OF METADATA
 });
